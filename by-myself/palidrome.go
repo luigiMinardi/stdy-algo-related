daf8d63d30678dc0ac08903affa1a7e0fc3943ec
Luigi <lugminsinlag@gmail.com>
2024-02-09 15:16:01 +0100

    refactor: Making the palindrome.go work with palindrome phrases and also improving readability of the algorithm and cleaning unnecessary print statements.

--- by-myself/palidrome.go
+++ by-myself/palidrome.go
@@ -12,20 +12,19 @@ func main() {
 
     scan := bufio.NewScanner(file)
 
-    counter := 0
     for scan.Scan() {
-        counter = counter + 1
-        fmt.Println(counter)
         if scan.Text() == "" || len(scan.Text()) == 1 {
             fmt.Println(scan.Text(), true)
         } else {
-            for i, j := 0, len(scan.Text())-1; i < j; i,j = i+1, j-1 {
-                //fmt.Println(i,j, strings.ToLower(string(scan.Text()[i])) == strings.ToLower(string(scan.Text()[j])))
-                if  strings.ToLower(string(scan.Text()[i])) != strings.ToLower(string(scan.Text()[j])) {
+            line := strings.ToLower(strings.ReplaceAll(scan.Text(), " ", ""))
+
+            for i, j := 0, len(line)-1; i < j; i,j = i+1, j-1 {
+                if  line[i] != line[j] {
                      fmt.Println(scan.Text(), false)
                      break
                 }
-                if (j-1 == i || j-2 == i && len(scan.Text()) % 2 == 1) {
+                // reached half of even or odd word so can print true
+                if j-1 == i || (j-2 == i && len(line) % 2 == 1) {
                     fmt.Println(scan.Text(), true)
                 }
             }
